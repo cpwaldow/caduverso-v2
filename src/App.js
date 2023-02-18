0741370409c31512e6c49a09ed92598d51304983
Carlos Eduardo <cpwaldow@gmail.com>
2023-02-18 15:47:14 -0300

    roteamento para home, sobremim e 404

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Home from './pages/Home';
 import About from './pages/About';
-
-const paginas = window.location.pathname === '/' ? <Home /> : <About />;
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return paginas;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sobremim' element={<About />} />
+        <Route path='*' element={<div>página não encontrada</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
